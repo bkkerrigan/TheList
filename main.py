31d7a46b87afcd6f2d0f6bb2e7a9ae77d34de622
bkkerrigan <kerrigan.brendan@gmail.com>
2013-12-20 20:37:22 -0500

    Import facebook and make fql searches

--- main.py
+++ main.py
@@ -4,5 +4,10 @@ from Finder import Finder
 import facebook 
 
 finder = Finder()
+
 finder.searchRoom('0311E')
 
+graph = facebook.GraphAPI("atoken")
+results = graph.fql("SELECT id, username, name, pic_square FROM profile WHERE contains('brendan kerrigan') and type='user'")
+
+print results[0]
